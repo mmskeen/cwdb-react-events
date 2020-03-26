09161ec1dd9cbd55a2b434441410fe26e23f3b4a
Michael Skeen <mmskeen@gmail.com>
2020-03-26 12:40:43 -0600

    Change button color based on mouse movement

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [headingText, setHeadingText] = useState("Hello");
+  const [isMouseOver, setMouseOver] = useState(false);
+  function handleClick() {
+    setHeadingText("Submitted");
+  }
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+  function handleMouseLeave() {
+    setMouseOver(false);
+  }
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}
+        onMouseLeave={handleMouseLeave}
+        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+      >
+        Submit
+      </button>
     </div>
   );
 }
